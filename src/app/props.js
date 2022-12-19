2e572b7fd5ee8e9023fd29af254ffcddce9aba9c
mihirrana10 <109574636+mihirrana11@users.noreply.github.com>
2022-12-19 12:48:37 +0530

    add data in database

--- src/app/props.js
+++ src/app/props.js
@@ -7,11 +7,45 @@ import Sidebar from './sidebar';
 import { Link } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
 import DataTable from 'react-data-table-component';
-
+import { ModalHeader,Modal, ModalBody } from "reactstrap"; 
 
 
 export default function Props(props) {
+    const [refresh, setRefersh] = useState(false);
+
+  const [users, setUsers] = useState({
+    first_name: "",
+    last_name: "",
+    email: ""  
+    
+  });
+
+  const { first_name,last_name, email} = users;
+  const onInputChange = e => {
+    setUsers({ ...users, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+   alert("Done !!!");
+    //  swal("licked the Good job!", "You!", "success");
   
+    e.preventDefault();
+    const result = await axios.post("http://localhost/reactjs_project/inster.php", users);
+
+    setUsers(JSON.stringify(result));
+
+
+    //const obj = JSON.parse(result);
+
+    // if(JSON.stringify(result.data.success))  
+    // {
+    //   alert("Data saved success fully with the id "+result.data.newids);
+    // }
+    
+    // //navigate.push("/");
+  };
+
+
   
     const name = props.name;
     const tool = props.tool;
@@ -31,14 +65,65 @@ const Viewusers = async () => {
 
   useEffect(() => {
     Viewusers();
-  }, []);
+  }, [refresh]);
 
 // end of the view all data contnet
 
-
+const [model, setModel] = useState(false);
 
   return (
    <>
+  <Modal
+   size="sm"
+        isOpen={model}
+        toggle={()=>setModel(!model)}>
+
+    <ModalHeader
+         toggle={()=>setModel(!model)}>
+      Add User
+    </ModalHeader>
+    <ModalBody>
+    <form onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Name"
+              name="first_name"
+              value={first_name}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+        
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Username"
+              name="last_name"
+              value={last_name}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+   
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control form-control-lg"
+              placeholder="Enter Your E-mail Address"
+              name="email"
+              value={email}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+    
+          <button  className="btn btn-primary btn-block">Add User</button>
+        </form>
+    </ModalBody>
+
+  </Modal>
+
+  
          <div className="container-scroller">
     <Sidebar></Sidebar>
       <div className="container-fluid page-body-wrapper">
@@ -55,9 +140,14 @@ const Viewusers = async () => {
           </div>
           <div class="col-lg-12 grid-margin stretch-card">
                 <div class="card">
+                
                   <div class="card-body">
+                  
                     <h4 class="card-title">Data View</h4>
-                   
+                    <button className="btn mt-3" style={{backgroundColor:"White"}}  onClick={() =>setModel(true) } >
+                    Add User
+                   </button><br/>
+                  
                     <div class="table-responsive">
                       <table class="table table-striped">
                         <thead>
